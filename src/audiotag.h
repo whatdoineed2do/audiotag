7b38ab5d08553d598857cca74ea6415a461b1763
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2018-12-02 10:16:23 +0000

    use stderr for everything other than notice

--- src/audiotag.h
+++ src/audiotag.h
@@ -10,14 +10,14 @@ extern bool  _verbose;
 
 
 #define MP3_TAG_ERR(m)     std::cerr << AudioTag::_argv0 << ": [error]  " << m << std::endl;
-#define MP3_TAG_WARN(m)    std::cout << AudioTag::_argv0 << ": [warn]  " << m << std::endl;
+#define MP3_TAG_WARN(m)    std::cerr << AudioTag::_argv0 << ": [warn]  " << m << std::endl;
 #define MP3_TAG_NOTICE(m)  std::cout << AudioTag::_argv0 << ": " << m << std::endl;
 
-#define MP3_TAG_WARN_VERBOSE(m)    if (AudioTag::_verbose)  { std::cout << AudioTag::_argv0 << ": [warn]  " << m << std::endl; }
+#define MP3_TAG_WARN_VERBOSE(m)    if (AudioTag::_verbose)  { std::cerr << AudioTag::_argv0 << ": [warn]  " << m << std::endl; }
 #define MP3_TAG_NOTICE_VERBOSE(m)  if (AudioTag::_verbose)  { std::cout << AudioTag::_argv0 << ":  " << m << std::endl; }
 
 #ifdef DEBUG
-#define MP3_TAG_DEBUG(m)   std::cout << AudioTag::_argv0 << "; [DEBUG]  " << m << std::endl;
+#define MP3_TAG_DEBUG(m)   std::cerr << AudioTag::_argv0 << "; [DEBUG]  " << m << std::endl;
 #else
 #define MP3_TAG_DEBUG(m)  
 #endif
