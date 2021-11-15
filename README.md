744ead9805a62a8c8ec043ef7cb98483fdc39985
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2021-11-15 16:01:10 +0000

    add optional ffmpeg libavformat/avutil for more accurate file support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Command line multi audio format meta tagging util
 Command line utility based on `TagLib (v1.11.1)` to read/write basic meta information to the follow file formats:
 - MP3
 - flac
-- M4a (.m4a/.aac MP4 audio)
+- M4a (.m4a MP4 audio)
 
 Basic functionality to:
 - unified interface across all supported formats: goodbye to different tag names im metaflac vs id3tag etc fir simple tagging
@@ -12,6 +12,8 @@ Basic functionality to:
 - sync - copy existing tag (ie ID3v2) to another tag (ie APE) for MP3
 - delete - delete tags from file
 - UTF8 tag data supported in tags which support it (NOT id3v1)
+
+`ffmpeg/libavfilter` will allow for more accurate file determination based on actual file container type, rather than file extension.
 
 Based heavily on the `tagwritter` util from `TabLib` and previous mp3tag
 
--- src/File.cc
+++ src/File.cc
@@ -1,14 +1,32 @@
 #include "File.h"
 
+#include "../config.h"
+
 #include <errno.h>
+
+#ifdef HAVE_FFMPEG
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/opt.h>
+#include <libavutil/log.h>
+
+static void  _avlog_callback_null(void *ptr, int level, const char *fmt, va_list vl)
+{ }
+}
+static int  _ffmpeg_err = 0;
+
+#include <list>
+static std::list<const char*>  ffmpeg_supported_fmts;
+#endif
+
 
 namespace AudioTag
 {
 const char*  FileMP3::sffx[]     = { ".mp3", NULL };
 const char*  FileOGGFlac::sffx[] = { ".ogg", NULL };
 const char*  FileFlac::sffx[]    = { ".flac", NULL };
-/* drop .aac - for some reason ffmpeg writing to an .aac container causes this 
- * crash; same opts to .m4a is fine
+/* drop .aac - this is a raw stream with no container and thus no meta 
  */
 const char*  FileM4a::sffx[]     = { ".m4a", ".mp4", ".alac", NULL };  // ffmpeg likes to force alac into a .m4a
 
@@ -17,6 +35,116 @@ std::ostream& operator<<(std::ostream& os_, const File& f_)
     return f_.meta().out(os_, f_);
 }
 
+#ifdef HAVE_FFMPEG
+File*  _ffmpeg_create(const char* file_, MetaOut& mo_)
+{
+    av_log_set_flags(AV_LOG_SKIP_REPEATED);
+    av_log_set_callback(_avlog_callback_null);
+
+    if (ffmpeg_supported_fmts.empty()) {
+	const char**  fmts[] = {
+	    FileFlac::sffx,
+	    FileM4a::sffx,
+	    FileMP3::sffx,
+	    nullptr
+	};
+	const char*** pf = fmts;
+	while (*pf)
+	{
+	    const char** p = *pf++;
+	    while (*p) {
+		ffmpeg_supported_fmts.push_back(**p == '.' ? *p+1 : *p);
+		++p;
+	    }
+	}
+    }
+
+    int ret;
+
+    AVFormatContext*  ctx = NULL;
+    if ( (ret = avformat_open_input(&ctx, file_, NULL, NULL)) < 0) {
+	_ffmpeg_err = ret;
+	return NULL;
+    }
+
+    if ( (ret = avformat_find_stream_info(ctx, NULL)) < 0) {
+        avformat_close_input(&ctx);
+	_ffmpeg_err = ret;
+        return NULL;
+    }
+
+    bool  hasvideo = true;
+
+    File*  f = NULL;
+ 
+    // find the container is supported and then worry about the codec
+   auto  supported_container =  std::find_if(ffmpeg_supported_fmts.cbegin(), ffmpeg_supported_fmts.cend(), 
+	[&ctx](const char* val_) {
+	    return strstr(ctx->iformat->name, val_);
+	});
+
+    if (supported_container == ffmpeg_supported_fmts.cend()) {
+	// container not supported
+	_ffmpeg_err = AVERROR_DECODER_NOT_FOUND;
+    }
+    else
+    {
+	for (int i=0; i<ctx->nb_streams; ++i)
+	{
+	    switch (ctx->streams[i]->codecpar->codec_type)
+	    {
+		case AVMEDIA_TYPE_VIDEO:
+		{
+		    switch (ctx->streams[i]->codecpar->codec_id) {
+			case AV_CODEC_ID_MJPEG:
+			case AV_CODEC_ID_MJPEGB:
+			    // embedded artwork, not video
+			    hasvideo = false;
+			    break;
+
+			default:
+			    hasvideo = true;
+			    break;
+		    }
+		} break;
+
+		/* WARN -- only consider the file's FIRST audio stream - if normal 
+		 * siutations this is fine
+		 */
+		case AVMEDIA_TYPE_AUDIO:
+		{
+		    switch (ctx->streams[i]->codecpar->codec_id)
+		    {
+			case AV_CODEC_ID_MP3:
+			    f = new FileMP3(file_, mo_);
+			    break;
+
+			case AV_CODEC_ID_AAC:
+			case AV_CODEC_ID_ALAC:
+			    f = new FileM4a(file_, mo_);
+			    break;
+
+			case AV_CODEC_ID_FLAC:
+			    f = new FileFlac(file_, mo_);
+
+			default:
+			    break;
+		    }
+		} break;
+
+		default:
+		    break;
+	    }
+
+	    if (f) {
+		break;
+	    }
+	}
+    }
+    avformat_close_input(&ctx);
+    return f;
+}
+#endif
 
 File*  FileFactory::create(const char* f_, int& errno_, MetaOut& mo_)
 {
@@ -47,6 +175,10 @@ File*  FileFactory::create(const char* f_, int& errno_, MetaOut& mo_)
 
 
     File*  f = NULL;
+
+#ifdef HAVE_FFMPEG
+    f = _ffmpeg_create(f_, mo_);
+#else
     const char*  p;
     if ( (p = strrchr(f_, '.')) == NULL) {
         return NULL;
@@ -55,17 +187,24 @@ File*  FileFactory::create(const char* f_, int& errno_, MetaOut& mo_)
     std::string  tmp = p;
     std::transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
     p = tmp.c_str();
-
     if (f == NULL && _match(p,  FileMP3::sffx))  f = new FileMP3(f_, mo_);
     if (f == NULL && _match(p, FileFlac::sffx))  f = new FileFlac(f_, mo_);
     if (f == NULL && _match(p,  FileM4a::sffx))  f = new FileM4a(f_, mo_);
     //if (f == NULL && _match(p, FileOGGFlac::sffx))  f = new FileOGGFlac(f_, mo_);
+#endif
 
     return f;
 }
 
 const char*  FileFactory::what(int err_)
 {
+#ifdef HAVE_FFMPEG
+    if (_ffmpeg_err) {
+	static char  ffmpeg_errstr[1024] = { 0 };
+	av_strerror(_ffmpeg_err, ffmpeg_errstr, 1024);
+	return ffmpeg_errstr;
+    }
+#endif
     switch (err_)
     {
         case 0:  return "unknown/unhandled file format";
